aaa2f4d08bcd40edb551177a2db02af98b100286
ErwiseDevelopment <erwisedev@gmail.com>
2022-09-22 02:55:33 -0300

    ajuste solicitacao HHH-675967

--- template-parts/content-calendar.php
+++ template-parts/content-calendar.php
@@ -260,7 +260,7 @@
                                                             <div class="my-2">
                                                                         <p class="l-calendar__text u-font-weight-extrabold u-color-folk-primary mb-0">
                                                                             <!-- // 14 -->
-                                                                            // <?php echo var_dump( $dia_data); ?>.<?php echo $mes_data; ?></p>
+                                                                            // <?php echo var_dump( $dia_data); ?>.<?php echo var_dump( $mes_data); ?></p>
                                                                         </p>
 
                                                                         <p class="l-calendar__text u-font-weight-semibold mb-0">

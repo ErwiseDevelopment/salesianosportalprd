d6272fa6e09cb6c9361ceec0a4f63b81e7d9f223
ErwiseDevelopment <erwisedev@gmail.com>
2022-09-23 00:14:10 -0300

    ajuste solicitacao HHH-675967

--- template-parts/content-calendar.php
+++ template-parts/content-calendar.php
@@ -167,7 +167,7 @@
                                                 array(
                                                         'taxonomy' => 'categoria-datas-especiais',
                                                         'field' => 'id',
-                                                        'terms' => 102,103,104,
+                                                        'terms' => array(102,103,104,105),
                                                 ),
                                             ),
                                         );
